5bc5f41097852b9545c13f865f75a7f6ebb6f4c5
Luis Picareli <lfpicareli@outlook.com>
2025-12-16 19:02:27 +0000

    Profile Class WIP

--- src/profile.py
+++ src/profile.py
@@ -1,13 +1,21 @@
-# from collections import namedtuple
+from dataclasses import dataclass
+from datetime import date
+from typing import NamedTuple
 
+
+class Entry (NamedTuple):
+    date_added : date
+    value : float
+
+@dataclass
 class Profile:
-    def __init__(self, height: float, target_weight: float, target_date: float):
-        self.height = height
-        self.target_weight = target_weight
-        self.target_date = target_date
-        self.bmi = None
-        self.max_weight = None
-        self.min_weight = None
+    height : float
+    target_weight : float
+    target_date : date
+    bmi : float
+    max_weight : float
+    min_weight : float
+    entries : Entry
 
     def calculate_bmi (self, height: float, weight: float):
         return height / (weight ** 2)
